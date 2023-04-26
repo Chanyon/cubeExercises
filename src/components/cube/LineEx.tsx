e4de71b493557733e656cd91d24f9884c9ae47a8
dcyun <Dominic_Duan@163.com>
2023-04-27 01:19:36 +0800

    Update

--- src/components/cube/LineEx.tsx
+++ src/components/cube/LineEx.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef } from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Input, Textarea, Text } from "@chakra-ui/react";
 import { Main } from "./rubik/src/main.js";
 import "./LineEx.css";
 
 function Cube() {
+  const [value, setValue] = React.useState('')
+
   const divRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const cube = new Main();
@@ -13,15 +15,25 @@ function Cube() {
       cube.resetRotateParams();
     }
   }, []);
-
+  
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    let inputValue = e.target.value
+    setValue(inputValue)
+  }
   return (
-    <Flex w="80%" h="830px" bg="#ffffff">
-      <Box w="60%">1</Box>
-      <Box w="40%">
+    <Flex w="80%" h="800px" bg="#ffffff">
+      <Box w="60%" color="blackAlpha.800" fontWeight="bold" p={3}>
+        <Box><Text m="8px">棱块编码：</Text><Input m="8px"/></Box>
+        <Box><Text m="8px">角块编码：</Text><Input m="8px"/></Box>
+        <Box><Text m="8px">棱块翻色：</Text><Input m="8px"/></Box>
+        <Box><Text m="8px">角块翻色：</Text><Input m="8px"/></Box>
+        <Box><Text m="8px">还原步骤：</Text><Textarea rows={6} isInvalid onChange={handleInputChange} size="lg" m="8px" placeholder="输入公式"></Textarea></Box>
+      </Box>
+      <Box w="40%" display="flex" justifyContent="center" alignItems="center">
         <div ref={divRef} id="retina"></div>
       </Box>
     </Flex>
   );
 }
 
-export{ Cube }
+export { Cube }
