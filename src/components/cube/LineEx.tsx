8668b796943dfc34a59d4ad5b12a954e4db664d5
dcyun <Dominic_Duan@163.com>
2023-04-29 21:13:16 +0800

    Up

--- src/components/cube/LineEx.tsx
+++ src/components/cube/LineEx.tsx
@@ -3,31 +3,49 @@ import { useHotkeys } from "react-hotkeys-hook";
 import { Box, Flex, Input, Textarea, Text } from "@chakra-ui/react";
 import { Main } from "./rubik/src/main.js";
 import "./LineEx.css";
-import { string2FormulaArr } from "../../utils/str2Formula.js";
 
 function Cube() {
-  const steps = {
+  const deleteSteps = {
     "R": "R'",
+    "T": "R",
     "L": "L'",
     "F": "F'",
     "B": "B'",
     "U": "U'",
     "D": "D'",
-    "L'": "L",
-    "F'": "F",
-    "B'": "B",
-    "U'": "U",
-    "D'": "D",
-    "R2": "R2",
-    "L2": "L2",
-    "F2": "F2",
-    "B2": "B2",
-    "U2": "U2",
-    "D2": "D2",
+    "K": "L",
+    "G": "F",
+    "V": "B",
+    "I": "U",
+    "C": "D",
     "M": "M'",
     "S": "S'",
     "E": "E'",
+    "W": "E",
+    "A": "S'",
+    "N": "M",
   };
+  const alias = {
+    "R": "R",
+    "L": "L",
+    "F": "F",
+    "B": "B",
+    "U": "U",
+    "D": "D",
+    "K": "L'",
+    "G": "F'",
+    "V": "B'",
+    "I": "U'",
+    "C": "D'",
+    "M": "M",
+    "S": "S",
+    "E": "E",
+    "W": "E'",
+    "A": "S'",
+    "N": "M'",
+    "T": "R'",
+  };
+
   const keys = [{ f: "u => U", r: "i => U'" },
   { f: "r => R", r: "t => R'" }, { f: "l => L", r: "k => L'" }, { f: "f => F", r: "g => F'" },
   { f: "b => B", r: "v => B'" }, { f: "d => D", r: "c => D'" }, { f: "m => M", r: "n => M'" },
@@ -35,7 +53,6 @@ function Cube() {
   ];
   const [stepValue, setStepValue] = useState("");
   const [prevSteps, setPrevSteps] = useState<Array<string>>([]);
-  const [currentStep, setCurrentStep] = useState(0);
   const [cube, setCube] = useState<any>(null);
   const divRef = useRef<HTMLDivElement>(null);
 
@@ -53,147 +70,139 @@ function Cube() {
   //键盘快捷键
   {
     useHotkeys("u,i,r,t,f,g,l,k,e,w,s,a,d,c,b,v,m,n", (event, handler) => {
-      switch (handler.keys?.pop()) {
-        case "u":
-          if (cube) {
-            cube.keyboardMove(["U"]);
+      if (event.type === "keyup") { 
+        setTimeout(() => {
+          switch (handler.keys?.pop()) {
+            case "u":
+              if (cube) {
+                cube.keyboardMove(["U"]);
+              }
+              break;
+            case "i":
+              if (cube) {
+                cube.keyboardMove(["U'"]);
+              }
+              break;
+            case "r":
+              if (cube) {
+                cube.keyboardMove(["R"]);
+              }
+              break;
+            case "t":
+              if (cube) {
+                cube.keyboardMove(["R'"]);
+              }
+              break;
+            case "l":
+              if (cube) {
+                cube.keyboardMove(["L"]);
+              }
+              break;
+            case "k":
+              if (cube) {
+                cube.keyboardMove(["L'"]);
+              }
+              break;
+            case "f":
+              if (cube) {
+                cube.keyboardMove(["F"]);
+              }
+              break;
+            case "g":
+              if (cube) {
+                cube.keyboardMove(["F'"]);
+              }
+              break;
+            case "d":
+              if (cube) {
+                cube.keyboardMove(["D"]);
+              }
+              break;
+            case "c":
+              if (cube) {
+                cube.keyboardMove(["D'"]);
+              }
+              break;
+            case "b":
+              if (cube) {
+                cube.keyboardMove(["B"]);
+              }
+              break;
+            case "v":
+              if (cube) {
+                cube.keyboardMove(["B'"]);
+              }
+              break;
+            case "m":
+              if (cube) {
+                cube.keyboardMove(["M"]);
+              }
+              break;
+            case "n":
+              if (cube) {
+                cube.keyboardMove(["M'"]);
+              }
+              break;
+            case "e":
+              if (cube) {
+                cube.keyboardMove(["E"]);
+              }
+              break;
+            case "w":
+              if (cube) {
+                cube.keyboardMove(["E'"]);
+              }
+              break;
+            case "s":
+              if (cube) {
+                cube.keyboardMove(["S"]);
+              }
+              break;
+            case "a":
+              if (cube) {
+                cube.keyboardMove(["S'"]);
+              }
+              break;
+            default:
+              break;
           }
-          break;
-        case "i":
-          if (cube) {
-            cube.keyboardMove(["U'"]);
-          }
-          break;
-        case "r":
-          if (cube) {
-            cube.keyboardMove(["R"]);
-          }
-          break;
-        case "t":
-          if (cube) {
-            cube.keyboardMove(["R'"]);
-          }
-          break;
-        case "l":
-          if (cube) {
-            cube.keyboardMove(["L"]);
-          }
-          break;
-        case "k":
-          if (cube) {
-            cube.keyboardMove(["L'"]);
-          }
-          break;
-        case "f":
-          if (cube) {
-            cube.keyboardMove(["F"]);
-          }
-          break;
-        case "g":
-          if (cube) {
-            cube.keyboardMove(["F'"]);
-          }
-          break;
-        case "d":
-          if (cube) {
-            cube.keyboardMove(["D"]);
-          }
-          break;
-        case "c":
-          if (cube) {
-            cube.keyboardMove(["D'"]);
-          }
-          break;
-        case "b":
-          if (cube) {
-            cube.keyboardMove(["B"]);
-          }
-          break;
-        case "v":
-          if (cube) {
-            cube.keyboardMove(["B'"]);
-          }
-          break;
-        case "m":
-          if (cube) {
-            cube.keyboardMove(["M"]);
-          }
-          break;
-        case "n":
-          if (cube) {
-            cube.keyboardMove(["M'"]);
-          }
-          break;
-        case "e":
-          if (cube) {
-            cube.keyboardMove(["E"]);
-          }
-          break;
-        case "w":
-          if (cube) {
-            cube.keyboardMove(["E'"]);
-          }
-          break;
-        case "s":
-          if (cube) {
-            cube.keyboardMove(["S"]);
-          }
-          break;
-        case "a":
-          if (cube) {
-            cube.keyboardMove(["S'"]);
-          }
-          break;
-        default:
-          break;
+        }, 100);
       }
-    });
+    },{keyup: true});
     //todo x,y,z,x',y',z'
   }
 
-  // useEffect(() => {
-  //   const stepArr = string2FormulaArr(stepValue.toUpperCase());
-  //   const keyDelete = (e: KeyboardEvent) => {
-  //     if (e.key === "Backspace") {
-  //       if (prevSteps.length > 0) {
-  //         const peek = prevSteps.pop();
-  //         const val = steps[peek as keyof typeof steps];
-  //         if (cube && val) {
-  //           cube.keyboardMove([val]);
-  //         }
-  //       }
-  //     }
-  //   };
-  //   if (stepArr.length >= prevSteps.length + 2) {
-  //     setPrevSteps([...stepArr]);
-  //   } else {
-  //     // delete a step
-  //     // if (prevSteps.length > 0) {
-  //     //   const peek = prevSteps.pop();
-  //     //   const val = steps[peek as keyof typeof steps];
-  //     //   if (cube && val) {
-  //     //     cube.keyboardMove([val]);
-  //     //   }
-  //     // }
-  //     document.addEventListener("keydown", keyDelete);
-  //   }
+  useEffect(() => {
+    const stepArr = stepValue.toUpperCase().split("");
+    
+    if (stepArr.length >= prevSteps.length) {
+      setPrevSteps([...stepArr]);
+    } else {
+      // delete a step
+      if (prevSteps.length > 0) {
+        const peek = prevSteps.pop();
+        const val = deleteSteps[peek as keyof typeof deleteSteps];
+        if (cube && val) {
+          cube.keyboardMove([val]);
+        }
+      }
+    }
 
-  // }, [stepValue]);
+  }, [stepValue]);
 
-  // useEffect(() => {
-  //   if (cube && prevSteps.length > 0) {
-  //     const step = prevSteps[prevSteps.length - 1];
-  //     const step2 = prevSteps[prevSteps.length - 2];
-  //     if (myIncludes(step) && myIncludes(step2)) {
-  //       cube.keyboardMove([step2, step]);
-  //     }
-  //   }
-  // }, [prevSteps]);
+  useEffect(() => {
+    if (cube && prevSteps.length > 0) {
+      const step = prevSteps[prevSteps.length - 1];
+      if (myIncludes(step)) {
+        cube.keyboardMove([alias[step as keyof typeof alias]]);
+      }
+    }
+  }, [prevSteps]);
 
-  // const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-  //   const inputValue = e.target.value;
-  //   setStepValue(inputValue);
-  // }
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const inputValue = e.target.value;
+    setStepValue(inputValue);
+  }
 
   return (
     <Flex w="80%" h="860px" bg="#ffffff">
@@ -202,11 +211,11 @@ function Cube() {
         <Box><Text m="8px">角块编码：</Text><Input m="8px" /></Box>
         <Box><Text m="8px">棱块翻色：</Text><Input m="8px" /></Box>
         <Box><Text m="8px">角块翻色：</Text><Input m="8px" /></Box>
-        <Box><Text m="8px">还原步骤：</Text><Textarea rows={6} isInvalid size="lg" m="8px" placeholder=""></Textarea></Box>
+        <Box><Text m="8px">还原步骤：</Text><Textarea rows={6} onChange={handleInputChange}  isInvalid size="lg" m="8px" placeholder=""></Textarea></Box>
         <Box>
           <Text>快捷键：</Text>
           {
-            keys.map(item => (<Text>{item.f} / {item.r}</Text>))
+            keys.map((item,idx)=> (<Text key={idx}>{item.f} / {item.r}</Text>))
           }
         </Box>
       </Box>
@@ -218,7 +227,7 @@ function Cube() {
 }
 
 function myIncludes(step: string) {
-  const allStep = ["R", "U", "D", "L", "F", "B", "R2", "U2", "D2", "L2", "F2", "B2", "R'", "U'", "D'", "L'", "F'", "B'", "E", "S", "M", "E'", "S'", "M'", "E2", "S2", "M2"];
+  const allStep = ["R", "U", "D", "L", "F", "B", "T", "I", "C", "K", "G", "V", "E", "S", "M", "W", "A", "N"];
   return allStep.includes(step);
 }
 
