046dbd2fe0498232b10b3d277836efb5ebe701e6
dcyun <Dominic_Duan@163.com>
2023-06-17 00:32:21 +0800

    Add website link

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -11,6 +11,7 @@ function Home() {
 		{ name: "Cuber-天下雨", url: "https://space.bilibili.com/432235186" },
 		{ name: "Lietice", url: "https://space.bilibili.com/384234355" }],
 		rubik_math: [{ name: "大毛忽洞", url: "https://space.bilibili.com/1052676786" }],
+		cube_website: [{name: "cubealgtrainer", url: "https://eelpi.github.io/cubealgtrainer/"}],
 	};
 	return (
 		<>
@@ -34,6 +35,13 @@ function Home() {
 					<Text>魔方和数学：</Text>
 					{
 						homeInfo.rubik_math.map((item, idx) => (<Text key={idx}>{item.name}：<Link target="_blank" href={item.url}>{item.url}</Link></Text>
+						))
+					}
+				</Box>
+				<Box my="10px">
+					<Text>魔方网站：</Text>
+					{
+						homeInfo.cube_website.map((item, idx) => (<Text key={idx}>{item.name}：<Link target="_blank" href={item.url}>{item.url}</Link></Text>
 						))
 					}
 				</Box>
